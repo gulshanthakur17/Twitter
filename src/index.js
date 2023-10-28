54db6b6536f212e8b69d646f4e29afe4211ebdec
Gulshan Thakur <gulshanthakur205@gmail.com>
2023-10-28 22:05:07 +0530

    Added Passport Logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport'
 
 
 import { connect } from './config/databse.js';
+
+import { passportAuth }  from './config/jwt-middleware.js';
 
 import apiRoutes from './routes/index.js';
 
@@ -13,6 +16,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
+
 
 app.use('/api', apiRoutes);
 
